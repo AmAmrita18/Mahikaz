089969bc7421b9cbfda9241325d48f238b8ad583
Amrita Gupta <amamrita0818@gmail.com>
2024-03-12 22:46:01 +0530

    update tailwind config

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroAbout from './HeroAbout'
 import AboutInfo from './AboutInfo'
+import FindPlace from './FindPlace'
 
 const About = () => {
   return (
     <div>
       <HeroAbout/>
       <AboutInfo/>
+      <FindPlace/>
     </div>
   )
 }
