cea17943ee0e8f4bfa5f0e660cdc4eea8dc4e63b
Amrita Gupta <amamrita0818@gmail.com>
2024-03-18 13:34:39 +0530

    updated dropdown List

--- src/components/About/DropdownList.jsx
+++ src/components/About/DropdownList.jsx
@@ -1,67 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { RiArrowDropUpLine } from "react-icons/ri";
 
 const DropdownList = () => {
-  // const [selected, setIsSelected] = useState("Choose one");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenTwo, setIsOpenTwo] = useState(false);
+  const [isOpenThree, setIsOpenThree] = useState(false);
+  const [isOpenFour, setIsOpenFour] = useState(false);
   return (
     <div className="w-full">
-      <div className="w-[80%] h-full  max-w-[1200px] py-12 flex flex-row gap-0">
-        <div className="bg-white rounded-l-3xl border-r border-[#E4E4E4] px-16 py-10 flex items-start justify-start">
-          <select label="Looking For" 
-            name="lookingfor"
-            id="lookingfor"
-            className="border-none outline-none focus:outline-none outline-white"
-          >
-            <option value="option1">Buy</option>
-            <option value="option2">Rent</option>
-           
-          </select>
+      <div className="w-full h-full  max-w-[1200px] py-12 flex flex-row  ">
+        <div className="w-full rounded-l-3xl border-r-2 border-[#E4E4E4] bg-white">
+          <button onClick={() => setIsOpen((prev) => !prev)} className="bg-white py-12 pl-8 pr-8  text-start rounded-l-3xl flex flex-row gap-4 items-center text-[17px] leading-[19.58px] tracking-[-0.43px] text-black">
+            Looking For
+            {!isOpen ? <RiArrowDropDownLine className="text-[28px] font-thin" /> : <RiArrowDropUpLine className="text-[28px] font-thin text-black" />}
+          </button>
+          {isOpen && (
+            <ul className="bg-white w-[19%]  absolute z-10 mt-4 flex flex-col rounded-3xl text-start">
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-t-3xl w-full hover:bg-[#B2A797] ">Buy</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-b-3xl w-full hover:bg-[#B2A797] ">Rent</li>
+            </ul>
+          )}
+        </div>
+        <div className="w-full border-r-2 border-[#E4E4E4]  bg-white">
+          <button onClick={() => setIsOpenTwo((prev) => !prev)} className="bg-white py-12 pl-8 pr-8 text-start flex flex-row gap-4 items-center text-[17px] leading-[19.58px] tracking-[-0.43px] text-black">
+            Location
+            {!isOpenTwo ? <RiArrowDropDownLine className="text-[28px] font-thin" /> : <RiArrowDropUpLine className="text-[28px] font-thin text-black" />}
+          </button>
+          {isOpenTwo && (
+            <ul className="bg-white w-[19%]  absolute z-10 mt-4 flex flex-col rounded-3xl text-start">
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-t-3xl w-full hover:bg-[#B2A797] ">North</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 w-full hover:bg-[#B2A797] ">South</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 w-full hover:bg-[#B2A797] ">East</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 w-full hover:bg-[#B2A797] ">West</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-b-3xl w-full hover:bg-[#B2A797] ">Center</li>
+              
+            </ul>
+          )}
+        </div>
+        <div className="w-full border-r-2 border-[#E4E4E4]  bg-white">
+          <button onClick={() => setIsOpenThree((prev) => !prev)} className="bg-white py-12 pl-8 pr-8 text-start  flex flex-row gap-4 items-center text-[17px] leading-[19.58px] tracking-[-0.43px] text-black">
+            Property Type
+            {!isOpenThree ? <RiArrowDropDownLine className="text-[28px] font-thin" /> : <RiArrowDropUpLine className="text-[28px] font-thin text-black" />}
+          </button>
+          {isOpenThree && (
+            <ul className="bg-white w-[19%] absolute z-10 mt-4 flex flex-col rounded-3xl text-start">
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-t-3xl w-full hover:bg-[#B2A797] ">Apartment</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">House/Villa</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Land</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Office</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Commercial</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-b-3xl w-full hover:bg-[#B2A797] ">Residential Complex</li>
+            </ul>
+          )}
+        </div>
+        <div className="w-full rounded-r-3xl  bg-white">
+          <button onClick={() => setIsOpenFour((prev) => !prev)} className="bg-white py-12 pl-8 pr-8 text-start rounded-r-3xl flex flex-row gap-4 items-center text-[17px] leading-[19.58px] tracking-[-0.43px] text-black">
+            Budget
+            {!isOpenFour ? <RiArrowDropDownLine className="text-[28px] font-thin" /> : <RiArrowDropUpLine className="text-[28px] font-thin text-black" />}
+          </button>
+          {isOpenFour && (
+            <ul className="bg-white w-[19%]  absolute z-10 mt-4 flex flex-col rounded-3xl text-start">
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-t-3xl w-full hover:bg-[#B2A797] ">Rs 0 - Rs 25,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Rs 25,000 - Rs 50,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Rs 50,000 - Rs 75,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Rs 75,000 - Rs 100,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Rs 100,000 - Rs 150,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5  w-full hover:bg-[#B2A797] ">Rs 150,000 - Rs 200,000</li>
+              <li className="text-[17px] leading-[19.58px] tracking-[-0.43px] text-black px-8 py-5 rounded-b-3xl w-full hover:bg-[#B2A797] ">Rs 200,000 - Rs 1,000,000</li>
+            </ul>
+          )}
         </div>
 
-        <div className="bg-white border-r  border-[#E4E4E4] px-16 py-10 flex items-start justify-start">
-          <select
-          label="Location" 
-            name="location"
-            id="location"
-            className="border-none outline-none focus:outline-none outline-white"
-          >
-            
-            <option value="option1">North</option>
-            <option value="option2">South</option>
-            <option value="option3">East</option>
-            <option value="option4">West</option>
-            <option value="option4">Center</option>
-          </select>
-        </div>
-
-        <div className="bg-white border-r border-[#E4E4E4] px-16 py-10 flex items-start justify-start">
-          <select
-           label="Property Type"
-            name="propertytype"
-            id="propertytype"
-            className="border-none outline-none focus:outline-none outline-white"
-          >
-            
-            <option value="option1">Apartment</option>
-            <option value="option2">House/Villa</option>
-            <option value="option3">Land</option>
-            <option value="option4">Office</option>
-          </select>
-        </div>
-
-        <div className="bg-white rounded-r-3xl  px-16 py-10 flex items-start justify-start">
-          <select
-            label="Price"
-            name="price"
-            id="price"
-            className="border-none outline-none focus:outline-none outline-white"
-          >
-            
-            <option value="option1">Rs 0 - Rs 25,000</option>
-            <option value="option2">Rs 25,000 - Rs 50,000</option>
-            <option value="option3">Rs 50,000 - Rs 75,000</option>
-            <option value="option4">Rs 75,000 - Rs 100,000</option>
-          </select>
-        </div>
       </div>
     </div>
   );
--- src/components/PropertiesPage/AllProperties.jsx
+++ src/components/PropertiesPage/AllProperties.jsx
@@ -2,55 +2,67 @@ import React from "react";
 import Header from "../Header";
 import DropdownList from "../About/DropdownList";
 import { estates } from "../About/FindPlaceArr";
-import { FaBed, FaHashtag } from 'react-icons/fa6'
-import { FaBath } from 'react-icons/fa'
+import { FaBed, FaHashtag } from "react-icons/fa6";
+import { FaBath } from "react-icons/fa";
 import infoImage from "/footerBG.png";
 import { Link } from "react-router-dom";
 const AllProperties = () => {
   return (
-    <div className="w-full" 
-    style={{
+    <div
+      className="w-full"
+      style={{
         backgroundImage: `url('${infoImage}')`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         objectFit: "cover",
-      }}>
-        <div className="w-full h-full bg-[#937D6480]">
-      <Header transparent={true} />
-      
-      
-      <div className="w-[80%] h-full mx-auto max-w-[1200px] py-28">
-       
-          <h1 className='text-[50px] text-center pb-4 text-black leading-[57.6px] tracking-[-1.26px]'>All Properties</h1>
-          <h2 className='text-[16px] text-center  text-black leading-[24px] '>124 results</h2>
-          <DropdownList/>
-         <div className='grid lg:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-x-12 gap-y-8'>
-         {
-            estates.map((est) => (
-                <Link to="/mainproperty" key={est.id}>
-              <div key={est.id} className="flex flex-col shadow-2xl  bg-[#fff] rounded-[32px]">
-                <img src={est.thumbnail} alt="thumbnail" className='w-full h-52 rounded-t-[30px] brightness-50' />
-                <h3 className='text-black text-[24px] p-8 leading-[27.65px] tracking-[-0.6px] border-b border-[#E4E4E4]'>{est.title}</h3>
-                <div className="flex justify-between w-full rounded-b-[32px] text-black">
-                  <p className='flex gap-x-2 py-3  px-8 items-center'>
-                    <FaBed />
-                    {est.beds}
-                  </p>
-                  <p className='flex gap-x-2 py-4  px-8 items-center border-l border-r border-[#E4E4E4]'>
-                    <FaBath />
-                    {est.washroom}
-                  </p>
-                  <p className='flex gap-x-2 py-3  md:px-8 px-4 items-center'>
-                    <FaHashtag />
-                    {est.livingroom}
-                  </p>
+      }}
+    >
+      <div className="w-full h-full bg-[#937D6480]">
+        <Header transparent={true} />
+
+        <div className="w-[80%] h-full mx-auto max-w-[1200px] py-28">
+          <h1 className="text-[50px] text-center pb-4 text-black leading-[57.6px] tracking-[-1.26px]">
+            All Properties
+          </h1>
+          <h2 className="text-[16px] text-center  text-black leading-[24px] ">
+            124 results
+          </h2>
+          <DropdownList />
+          <div className="grid lg:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-x-12 gap-y-8">
+            {estates.map((est) => (
+              <Link to="/mainproperty" key={est.id}>
+                <div
+                  key={est.id}
+                  className="flex flex-col shadow-2xl  bg-[#fff] rounded-[32px]"
+                >
+                  <img
+                    src={est.thumbnail}
+                    alt="thumbnail"
+                    className="w-full h-52 rounded-t-[30px] brightness-50"
+                  />
+                  <h3 className="text-black text-[24px] p-8 leading-[27.65px] tracking-[-0.6px] border-b border-[#E4E4E4]">
+                    {est.title}
+                  </h3>
+                  <div className="flex justify-between w-full rounded-b-[32px] text-black">
+                    <p className="flex gap-x-2 py-3  px-8 items-center">
+                      <FaBed />
+                      {est.beds}
+                    </p>
+                    <p className="flex gap-x-2 py-4  px-8 items-center border-l border-r border-[#E4E4E4]">
+                      <FaBath />
+                      {est.washroom}
+                    </p>
+                    <p className="flex gap-x-2 py-3  md:px-8 px-4 items-center">
+                      <FaHashtag />
+                      {est.livingroom}
+                    </p>
+                  </div>
                 </div>
-              </div></Link>
-            ))
-          }
-         </div>
+              </Link>
+            ))}
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
